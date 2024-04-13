102f79cbb377d25a547ba6debfe28a07033e542b
Bryan Perez <bryaneperez@protonmail.com>
2024-04-12 22:59:55 -0400

    15 instructions complete

--- Chip8.hpp
+++ Chip8.hpp
@@ -22,9 +22,13 @@ class Chip8 {
     const unsigned int START_ADDRESS = 0x200;
     const unsigned int FONTSET_START = 0x50;
     const unsigned int FONTSET_SIZE = 80;
+
+    std::default_random_engine randGen;
+    std::uniform_int_distribution<uint8_t> randByte;
     
 
-    Chip8::Chip8(){
+    Chip8::Chip8() : randGen(std::chrono::system_clock::now().time_since_epoch().count())
+    {
         //Things here need to happen first.
         pc = START_ADDRESS;
 
@@ -46,10 +50,13 @@ class Chip8 {
 	    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
 	    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
     };
-
+        //Load fontset into memory.
         for (unsigned int i = 0; i < 80; i++){
             memory[FONTSET_START + i] = fontset[i];
         }
+
+        //Seed RNG
+        randByte = std::uniform_int_distribution<uint8_t>(0, 255U);
     }
 
     void Chip8::LoadROM(char const* filename){
@@ -74,5 +81,154 @@ class Chip8 {
             delete[] buffer;
         }
     }
+
+    //Instructions for Chip-8 begin here.
+
+    //Clear display (CLS)
+    void Chip8::OP_00E0(){
+        memset(video, 0, sizeof(video));
+    }
+
+    //Return from a subroutine(RET)
+    void Chip8::OP_00EE(){
+         sp--;
+         pc = stack[sp];
+    }
+
+    //Jump to a location (nnn)
+    void Chip8::OP_1nnn(){
+        uint16_t address = opcode & 0x0FFFu;
+        pc = address;
+    }
+
+    //Call subroutine at nnn
+    void Chip8::OP_2nnn(){
+        uint16_t address = opcode & 0x0FFFu;
+        stack[sp] = pc;
+        sp++;
+        pc = address;
+    }
+
+    //Skip next instruction if Vx == kk
+    void Chip8::OP_3xkk(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t byte = opcode & 0x0FFu;
+
+        if(registers[Vx] == byte){
+            pc += 2;
+        }
+    }
+
+    //Skip next instruction if Vx != kk
+    void Chip8::OP_4xkk(){
+        uint8_t Vx = (opcode &0x0F00u) >> 8u;
+        uint8_t byte = opcode & 0x00FFu;
+
+        if(registers[Vx] != byte){
+            pc += 2;
+        }
+    }
+
+    //Skip next instruction if Vx == Vy
+    void Chip8::OP_5xy0(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        if(registers[Vx] == registers[Vy]){
+            pc += 2;
+        }
+    }
+
+    //Set Vx to kk
+    void Chip8::OP_6xkk(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t byte = opcode & 0x00FFu;
+
+        registers[Vx] = byte;
+    }
+
+    //Add Vx and kk values
+
+    void Chip8::OP_7xkk(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t byte = opcode & 0x00FFu;
+
+        registers[Vx] += byte;
+    }
+
+    //Sets Vx to Vy
+    void Chip8::OP_8xy0(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        registers[Vx] = registers[Vy];
+    }
+
+    //Set Vx or Vy
+    void Chip8::OP_8xy1(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        registers[Vx] |= registers[Vy];
+    }
+
+    //Set Vx to Vx AND Vy
+    void Chip8::OP_8xy2(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        registers[Vx] &= registers[Vy];
+    }
+
+    //Set VX = Vx XOR Vy
+    void Chip8::OP_8xy3(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        registers[Vx] ^= registers[Vy];
+    }
+
+    //Add Vx and Vy, with carry value Vf
+    void Chip8::OP_8xy4(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+        uint16_t sum = registers[Vx] + registers[Vy];
+
+        if (sum > 255U){
+            registers[0xF] = 1;
+        }
+        else{
+            registers[0xF] = 0;
+        }
+
+        registers[Vx] = sum & 0xFFu;    
+    }
+
+    //Subtract Vx and Vy, Vf set to not borrow.
+    void Chip8::OP_8xy5(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        if(registers[Vx] > registers[Vy]) {
+            registers[0xF] = 1;
+        }
+        else {
+            registers[0xF] = 0;
+        }
+
+        registers[Vx] -= registers[Vy];
+    }
+
+    //If the least sig bit of Vx is 1, Vf gets set to 1. Vx is divided by 2.
+    void Chip8::OP_8xy6(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+        registers[0xF] = (registers[Vx] & 0x1u);
+        registers[Vx] >>= 1;
+    }
+
+    
+
+
 
 };
