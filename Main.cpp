bffb1543936dd9a9d7ea3c5fecff345e6c0232e1
Bryan Perez <bryaneperez@protonmail.com>
2024-04-16 19:58:17 -0400

    Platform header finished

--- Main.cpp
+++ Main.cpp
@@ -4,11 +4,39 @@
 */
 
 
-
+#include <chrono>
 #include <iostream>
+#include <string>
 #include "Chip8.hpp"
+#include "Platform.hpp"
 
-int main (int argc, char* args[]){
+//Width: 64, Height: 32
+
+int main (int argc, char* argv[]){
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <Scale> <Delay> <ROM>\n";
+        std::exit(EXIT_FAILURE);
+    }
+
+    int videoScale = std::stoi (argv[1]);
+    int cycleDelay = std::stoi (argv[2]);
+    char const* romFilename = argv[3];
+
+    Platform platform("CHIP-8 Emulator", 64 * videoScale, 32 * videoScale, 64, 32);
+    Chip8 chip8;
+    chip8.LoadROM(romFilename);
+    int videoPitch = sizeof(chip8.video[0]) * 64;
+
+    auto lastCycleTime = std::chrono::high_resolution_clock::now();
+    bool quit = false;
+
+    while (!quit){
+        quit = platform.ProcessInput(chip8.keypad);
+
+        auto currentTime = std::chrono::high_resolution_clock::now();
+        float dt = std::chrono::
+    }
+
 
 
 }; 
